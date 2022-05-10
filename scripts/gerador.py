724cd17482555bdee4cc9e99237f9eb3ae59789f
Caique Theodoro <f000762@modal.net.br>
2022-05-10 18:18:12 -0300

    Atualiza

--- scripts/gerador.py
+++ scripts/gerador.py
@@ -5,20 +5,20 @@ Importante: Eh necessario ter o ffpmeg no PATH
 """
 from pydub import AudioSegment
 
-ROOT = r'U:/DEPTO/DTVM RISCO/CONTROLES/SONS/scripts'
+ROOT = r'U:\\DEPTO\\DTVM RISCO\\CONTROLES\\SONS\\noisy-risk.github.io\\scripts'
 
 OUT_PATH, IN_PATH= f'{ROOT}/out', f'{ROOT}/in'
 
-source_sound = AudioSegment.from_mp3(f"{IN_PATH}/pesadelinho.mp3")
+source_sound = AudioSegment.from_mp3(f"{IN_PATH}/jony.mp3")
 
-_start_min, _start_sec = 3, 15
-_end_min, _end_sec = 3, 21
+_start_min, _start_sec = 1, 48
+_end_min, _end_sec = 1, 53
 
 _start = _start_min * 60 * 1000 + _start_sec * 1000
 _end  = _end_min * 60 * 1000 + _end_sec * 1000
 
 extract = source_sound[_start:_end]
 
-out_file = 'batata_batata'
+out_file = 'nois_eh_ruim'
 
 extract.export(f"{OUT_PATH}/{out_file}.mp3", format="mp3")
